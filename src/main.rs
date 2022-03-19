e684bb1ab4f17c78b542751df34f8ebec15ad562
net-runner <slazynski.j@gmail.com>
2022-03-19 16:02:59 +0100

    Finishd.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,43 @@
-use rand::distributions::{Distribution, Uniform};
-use std::io;
+use std::collections::HashMap;
+use std::time::Instant;
 
-fn is_prime(number: u32) -> bool {
-    if number > 1 {
-        let step = Uniform::new(2, number);
-        let mut rng = rand::thread_rng();
-        for _x in [0..3] {
-            let randomNumber = step.sample(&mut rng) - 1;
-            if (u32::pow(randomNumber, number - 1) % number != 1) {
-                return false;
-            }
-        }
-        return true;
+fn solve(a: Vec<i64>, b: Vec<i64>) -> Vec<i64> {
+    let mut c = Vec::new();
+    let mut n_set: HashMap<i64, i64> = HashMap::new();
+
+    for num in b {
+        let count = n_set.entry(num).or_insert(0);
+        *count += 1;
     }
-    return false;
+    for num in a {
+        let count = n_set.entry(num).or_insert(0);
+        if !is_prime(&count) {
+            c.push(num);
+        }
+    }
+    return c;
+}
+fn is_prime(number: &i64) -> bool {
+    if number <= &1 {
+        return false;
+    }
+    let stop = ((*number as f64).sqrt() + 1.0) as i64;
+    for i in 2..stop {
+        if number % i == 0 {
+            return false;
+        }
+    }
+    return true;
 }
 fn main() {
-    println!("Hello, world!");
+    let a = vec![2, 3, 9, 2, 5, 1, 3, 7, 10];
+    let b = vec![2, 1, 3, 4, 3, 10, 6, 6, 1, 7, 10, 10, 10];
+
+    let before = Instant::now();
+    let c = solve(a, b);
+
+    // C=[2,9,2,5,7,10]
+    println!("C: {:?}", c);
+
+    println!("Elapsed time: {:.2?}", before.elapsed());
 }
